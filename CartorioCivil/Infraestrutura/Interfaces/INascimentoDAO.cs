4458cd692dd874b229f54785a1994a837e717fef
FelipeFerraz98 <felipeferraz1398@gmail.com>
2025-04-08 20:54:01 -0300

    :sparkles: feat: Adicionado IRegistroDAO e INascimentoDAO

---
+++ CartorioCivil/Infraestrutura/Interfaces/INascimentoDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CartorioCivil.Entidades;
+
+namespace CartorioCivil.Infraestrutura.Interfaces
+{
+    public interface INascimentoDAO : IRegistroDAO<Nascimento>
+    {
+        Task<List<Nascimento>> ObterPorPeriodoAsync(DateTime dataInicio, DateTime dataFim);
+    }
+}
