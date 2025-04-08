3479e1f5c1fd67463115f483ec02b667528a5c00
FelipeFerraz98 <felipeferraz1398@gmail.com>
2025-04-08 15:05:46 -0300

    :sparkles: feat: Adicionado Entidades

---
+++ CartorioCivil/Entidades/Nascimento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CartorioCivil.Entidades
+{
+    public class Nascimento
+    {
+        public int Id { get; set; }
+        public DateTime DataRegistro { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string NomeRegistrado { get; set; }
+        public string NomePai { get; set; }
+        public string NomeMae { get; set; }
+        public DateTime DataNascimentoPai { get; set; }
+        public DateTime DataNascimentoMae { get; set; }
+        public string CpfnPai { get; set; }
+        public string CpfnMae { get; set; }
+    }
+}
