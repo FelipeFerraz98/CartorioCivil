049dd04552afa3ea1c8027e655aac3193e722976
FelipeFerraz98 <felipeferraz1398@gmail.com>
2025-04-09 15:44:26 -0300

    :sparkles: feat: Adicionado IObitoDAO

---
+++ CartorioCivil/Infraestrutura/Interfaces/IObitoDAO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CartorioCivil.Entidades;
+
+namespace CartorioCivil.Infraestrutura.Interfaces
+{
+    public interface IObitoDAO : IRegistroDAO<Obito>
+    {
+        Task<List<Obito>> ObterPorNomeAsync(string nome);
+    }
+}
