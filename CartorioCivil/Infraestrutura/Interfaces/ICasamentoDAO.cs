3978388f34a79d07365dd8e6394c3f7a28378e2d
FelipeFerraz98 <felipeferraz1398@gmail.com>
2025-04-09 15:45:23 -0300

    :sparkles: feat: Adicionado ICasamentoDAO

---
+++ CartorioCivil/Infraestrutura/Interfaces/ICasamentoDAO.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using CartorioCivil.Entidades;
+
+namespace CartorioCivil.Infraestrutura.Interfaces
+{
+    public interface ICasamentoDAO : IRegistroDAO<Casamento>
+    {
+        Task<Casamento> ObterPorIdConjugueAsync(string idConjuge);
+    }
+}
