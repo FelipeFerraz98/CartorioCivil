f4e45cb6f451591cdc20afbd92ba554f38108f09
FelipeFerraz98 <felipeferraz1398@gmail.com>
2025-04-08 23:00:13 -0300

    :sparkles: feat: Adicionado CasamentoDAO

---
+++ CartorioCivil/Infraestrutura/RegistrosDAO/CasamentoDAO.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Npgsql;
+using CartorioCivil.Entidades;
+using CartorioCivil.Infraestrutura.Interfaces;
+using CartorioCivil.Infraestrutura.BancoDeDados;
+using System.Linq;
+
+namespace CartorioCivil.Infraestrutura.RegristrosDAO
+{
+    public class CasamentoDAO : IRegistroDAO<Casamento>
+    {
+        private readonly ConexaoDB _conexaoBanco;
+
+        public CasamentoDAO()
+        {
+            _conexaoBanco = new ConexaoDB();
+        }
+
+        public async Task AdicionarAsync(Casamento casamento)
+        {
+            string consulta = @"
+                INSERT INTO Casamento (DataRegistro, DataCasamento, IdConjuge1, IdConjuge2)
+                VALUES (@DataRegistro, @DataCasamento, @IdConjuge1, @IdConjuge2)";
+
+            var parametros = new Dictionary<string, object>
+            {
+                { "@DataRegistro", casamento.DataRegistro },
+                { "@DataCasamento", casamento.DataCasamento },
+                { "@IdConjuge1", casamento.IdConjuge1 },
+                { "@IdConjuge2", casamento.IdConjuge2 }
+            };
+
+            await _conexaoBanco.ExecutarComandoAsync(consulta, parametros);
+        }
+
+        public async Task AtualizarAsync(Casamento casamento)
+        {
+            string consulta = @"
+                UPDATE Casamento
+                SET DataRegistro = @DataRegistro,
+                    DataCasamento = @DataCasamento,
+                    IdConjuge1 = @IdConjuge1,
+                    IdConjuge2 = @IdConjuge2
+                WHERE Id = @Id";
+
+            var parametros = new Dictionary<string, object>
+            {
+                { "@Id", casamento.Id },
+                { "@DataRegistro", casamento.DataRegistro },
+                { "@DataCasamento", casamento.DataCasamento },
+                { "@IdConjuge1", casamento.IdConjuge1 },
+                { "@IdConjuge2", casamento.IdConjuge2 }
+            };
+
+            await _conexaoBanco.ExecutarComandoAsync(consulta, parametros);
+        }
+
+        public async Task RemoverAsync(int id)
+        {
+            string consulta = "DELETE FROM Casamento WHERE Id = @Id";
+            var parametros = new Dictionary<string, object>
+            {
+                { "@Id", id }
+            };
+
+            await _conexaoBanco.ExecutarComandoAsync(consulta, parametros);
+        }
+
+        public async Task<List<Casamento>> ObterTodosAsync()
+        {
+            string consulta = "SELECT * FROM Casamento";
+            return await _conexaoBanco.ExecutarConsultaAsync(consulta, MapearParametros);
+        }
+
+        public async Task<Casamento> ObterPorIdAsync(int id)
+        {
+            string consulta = "SELECT * FROM Casamento WHERE Id = @Id";
+            var parametros = new Dictionary<string, object>
+            {
+                { "@Id", id }
+            };
+
+            var resultados = await _conexaoBanco.ExecutarConsultaAsync(consulta, MapearParametros, parametros);
+            return resultados.FirstOrDefault();
+        }
+
+        public Casamento MapearParametros(NpgsqlDataReader leitor)
+        {
+            return new Casamento
+            {
+                Id = leitor.GetInt32(leitor.GetOrdinal("Id")),
+                DataRegistro = leitor.GetDateTime(leitor.GetOrdinal("DataRegistro")),
+                DataCasamento = leitor.GetDateTime(leitor.GetOrdinal("DataCasamento")),
+                IdConjuge1 = leitor.GetInt32(leitor.GetOrdinal("IdConjuge1")),
+                IdConjuge2 = leitor.GetInt32(leitor.GetOrdinal("IdConjuge2"))
+            };
+        }
+    }
+}
